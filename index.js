0cc11998adbd7695d5090628d8f28586b020a1f6
Pranit Maske <maskepranit@gmail.com>
2021-04-21 20:41:25 +0530

    - CORS Fix

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ connectDB();
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://spacexlaunchdashboard.herokuapp.com"
+  );
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   next();
 });
 
@@ -38,5 +44,5 @@ app.use("/api/v1/spacex", spacexRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on Port ${ PORT }`);
+  console.log(`Listening on Port ${PORT}`);
 });
