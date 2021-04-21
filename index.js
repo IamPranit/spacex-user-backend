bb18856b1c07b47b4801672252bf470f269259f1
Pranit Maske <maskepranit@gmail.com>
2021-04-21 20:38:02 +0530

    - CORS Fix

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ connectDB();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://spacexlaunchdashboard.herokuapp.com",
-    credentials: true,
-  })
-);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.use(cookieParser());
 
