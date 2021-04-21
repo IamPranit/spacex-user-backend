227d79f5e16d72c137d459fa2f2a2d81e796e715
Pranit Maske <maskepranit@gmail.com>
2021-04-21 20:24:22 +0530

    - Cors change origin URL

--- index.js
+++ index.js
@@ -15,27 +15,13 @@ connectDB();
 
 app.use(express.json());
 
-const  whitelist = ['http://localhost:3000', "https://spacexlaunchdashboard.herokuapp.com" ];
+app.use(
+  cors({
+    origin: "https://spacexlaunchdashboard.herokuapp.com/",
+    credentials: true,
+  })
+);
 
-const  corsOptions = {
-  credentials: true,
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-app.use(corsOptions);
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.options('*', cors());
 app.use(cookieParser());
 
 if ((process.env.NODE_ENV = "development")) {
